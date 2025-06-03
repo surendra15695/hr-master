7e5caeca768ec662683b2f9429dbaa96e43ab7b3
surendra15695 <104766418+surendra15695@users.noreply.github.com>
2025-06-03 23:30:45 +0530

    Add files via upload

---
+++ Application.WebApp/ClientApp/src/environments/environment.prod.ts
@@ -0,0 +1,32 @@
+export const environment = {
+
+  production: true,
+  
+  //apiurl: 'https://mrfhruat.azurewebsites.net/api',
+  //apppath: 'https://mrfhruat.azurewebsites.net:5001',
+  //apiKey: "embee.co.in",
+  //// UAT Email Link
+  //emailLink: "https://mrfhruat.azurewebsites.net/"
+
+  //For UAT AppGateway
+  //apiurl: 'https://mrfconnectuat.mrfindia.net/api',
+  //apppath: 'https://mrfconnectuat.mrfindia.net:5001',
+  //apiKey: "embee.co.in",
+  // UAT Email Link
+  //emailLink: "https://mrfconnectuat.mrfindia.net/"
+  
+  //For Production
+  //apiurl: 'https://mrfhrportalprod.azurewebsites.net/api',
+  //apppath: 'https://mrfhrportalprod.azurewebsites.net:5001',
+  //apiKey: "embee.co.in",
+  // Prod Email Link
+  //emailLink: "https://mrfhrportalprod.azurewebsites.net/"
+  
+  //For Production AppGateway
+  apiurl: 'https://mrfconnect.mrfindia.net/api',
+  apppath: 'https://mrfconnect.mrfindia.net:5001',
+  apiKey: "embee.co.in",
+  // PROD Email Link
+  emailLink: "https://mrfconnect.mrfindia.net/"
+
+};
